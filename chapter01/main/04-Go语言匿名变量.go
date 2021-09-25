c46c56e693034ccc68fde4488c34b6ea7443654e
wxc_dz <812150635@qq.com>
2021-09-25 17:15:22 +0800

    匿名变量函数优化

--- chapter01/main/04-Go语言匿名变量.go
+++ chapter01/main/04-Go语言匿名变量.go
@@ -2,12 +2,18 @@ package main
 
 import "fmt"
 
-func GetData() (int, int)  {
+func GetData() (int, int) {
 	return 100, 200
 }
 
-func main() {
+func demo401() {
 	a, _ := GetData()
 	_, b := GetData()
 	fmt.Println(a, b)
+}
+
+func main() {
+
+	fmt.Println("===========demo401==========")
+	demo401()
 }
